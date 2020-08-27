672b837bd7680b7333248f75a42633623b9222d7
Shivix <52997365+Shivix@users.noreply.github.com>
2020-08-27 16:29:27 +0100

    adjusted unit tests for better readability of outputs when using multiple suites

--- test/arrayTest.cpp
+++ test/arrayTest.cpp
@@ -1,19 +1,21 @@
 #include "../include/ShivLib/array.hpp"
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(array_iterator_test) // NOLINT
-{
-    
-}
+BOOST_AUTO_TEST_SUITE(array_test) // NOLINT(cert-err58-cpp)
+    BOOST_AUTO_TEST_CASE(array_iterator_test) // NOLINT(cert-err58-cpp)
+    {
 
-BOOST_AUTO_TEST_CASE(fillswap_test) // NOLINT
-{
-    ShivLib::array<int, 5> array1{0, 1, 2, 3, 4};
-    ShivLib::array<int, 5> array2{4, 3, 2, 1, 0};
-    ShivLib::array<int, 5> expectedArray1{4, 4, 4, 4, 4};
-    array1.swap(array2);
-    
-    BOOST_TEST(array1 == array2);
-    array2.fill(4);
-    BOOST_TEST(array2 == expectedArray1);
-}
+    }
+
+    BOOST_AUTO_TEST_CASE(fillswap_test) // NOLINT(cert-err58-cpp)
+    {
+        ShivLib::array<int, 5> array1{0, 1, 2, 3, 4};
+        ShivLib::array<int, 5> array2{4, 3, 2, 1, 0};
+        ShivLib::array<int, 5> expectedArray1{4, 4, 4, 4, 4};
+        array1.swap(array2);
+
+        BOOST_TEST(array1 == array2);
+        array2.fill(4);
+        BOOST_TEST(array2 == expectedArray1);
+    }
+BOOST_AUTO_TEST_SUITE_END() // NOLINT(cert-err58-cpp)
