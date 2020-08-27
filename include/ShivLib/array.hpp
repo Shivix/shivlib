cdf85dfa44f486eedb4da74fdd7a8524e34007b2
Shivix <52997365+Shivix@users.noreply.github.com>
2020-08-27 16:23:04 +0100

    implemented and tested fill and swap + added == and != overloads

--- include/ShivLib/array.hpp
+++ include/ShivLib/array.hpp
@@ -5,7 +5,7 @@
 #include <iterator>
 
 namespace ShivLib{
-    template<typename T, std::size_t size>
+    template<typename T, std::size_t numOfElems>
     class array{
     public:
         using value_type = T;
@@ -16,11 +16,27 @@ namespace ShivLib{
         using reference = T&;
         using const_reference = const T&;
 
-        T m_data[size];
+        T m_data[numOfElems];
+        
         constexpr T* 
         data() noexcept{
             return m_data;
         }
+        constexpr const T*
+        data() const noexcept{
+            return m_data;
+        }
+        
+        void 
+        fill(const value_type& input){
+            std::fill(begin(), end(), input);
+        }
+        
+        void 
+        swap(array other) noexcept{
+            std::swap_ranges(begin(), end(), other.begin());
+        }
+        
         // Iterators
         constexpr auto
         begin() noexcept{
@@ -48,31 +64,48 @@ namespace ShivLib{
         }
         constexpr auto
         end() noexcept{
-            return iterator(data() + size);
+            return iterator(data() + numOfElems);
         }
         constexpr auto
         end() const noexcept{
-            return const_iterator(data() + size);
+            return const_iterator(data() + numOfElems);
         }
         constexpr auto
         cend() const noexcept{
-            return const_iterator(data() + size);
+            return const_iterator(data() + numOfElems);
         }
         constexpr auto
         rend() noexcept{
-            return reverse_iterator(data() + size);
+            return reverse_iterator(data() + numOfElems);
         }
         constexpr auto
         rend() const noexcept{
-            return const_reverse_iterator(data() + size);
+            return const_reverse_iterator(data() + numOfElems);
         }
         constexpr auto
         crend() const noexcept{
-            return const_reverse_iterator(data() + size);
+            return const_reverse_iterator(data() + numOfElems);
         }
 
         // Capacity
-
+        [[nodiscard]] constexpr std::size_t
+        size() const noexcept{
+            return numOfElems;
+        }
+        [[nodiscard]] constexpr bool 
+        empty() const noexcept{
+            return size() == 0;
+        }
+        
+        // Comparison
+        constexpr bool 
+        operator == (const array& other) const{
+            return std::equal(begin(), end(), other.begin());
+        }
+        constexpr bool
+        operator != (const array& other) const{
+            return !(this == other);
+        }
     };
 }
 
--- test/arrayTest.cpp
+++ test/arrayTest.cpp
@@ -1,8 +1,19 @@
 #include "../include/ShivLib/array.hpp"
-#define BOOST_TEST_MODULE ShivLib arrayTest
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(_test) // NOLINT
+BOOST_AUTO_TEST_CASE(array_iterator_test) // NOLINT
 {
     
+}
+
+BOOST_AUTO_TEST_CASE(fillswap_test) // NOLINT
+{
+    ShivLib::array<int, 5> array1{0, 1, 2, 3, 4};
+    ShivLib::array<int, 5> array2{4, 3, 2, 1, 0};
+    ShivLib::array<int, 5> expectedArray1{4, 4, 4, 4, 4};
+    array1.swap(array2);
+    
+    BOOST_TEST(array1 == array2);
+    array2.fill(4);
+    BOOST_TEST(array2 == expectedArray1);
 }
