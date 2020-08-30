9a1ac8bb2eaa98582f580a142136998140153739
Shivix <52997365+Shivix@users.noreply.github.com>
2020-08-30 16:43:14 +0100

    added the start of range_based_test

--- test/vectorTest.cpp
+++ test/vectorTest.cpp
@@ -2,7 +2,12 @@
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(vector_test) // NOLINT(cert-err58-cpp)
-    BOOST_AUTO_TEST_CASE(test){ // NOLINT(cert-err58-cpp)
-        
+    BOOST_AUTO_TEST_CASE(range_based_test){ // NOLINT(cert-err58-cpp)
+
+        vector<int> vector1{2};
+
+        for(auto&& i: vector1){
+            
+        }
     }
 BOOST_AUTO_TEST_SUITE_END() // NOLINT(cert-err58-cpp)
