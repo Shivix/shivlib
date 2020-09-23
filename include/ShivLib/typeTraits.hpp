2ee4f5dd86dd2f3d0f2080ab919fa1c8d3bda9fe
Shivix <52997365+Shivix@users.noreply.github.com>
2020-09-23 11:00:34 +0100

    implemented integral constants

--- include/ShivLib/typeTraits.hpp
+++ include/ShivLib/typeTraits.hpp
@@ -2,6 +2,7 @@
 #define SHIVLIB_TYPETRAITS_HPP
 
 namespace ShivLib{
+    // remove refness of a type
     template<typename T>
     struct remove_reference{
         using type = T;
@@ -14,25 +15,34 @@ namespace ShivLib{
     struct remove_reference<T&&>{
         using type = T;
     };
+    // integral constants
+    template<typename T, T v>
+    struct integral_constant{
+        using value_type = T;
+        using type = integral_constant<T, v>;
+        
+        static constexpr T value = v;
+        
+        constexpr operator value_type() const noexcept{ // NOLINT implicit casts allowed
+            return value;
+        }
+        constexpr value_type operator() () const noexcept{ // allows struct to be used as functor
+            return value;
+        }
+    };
+    using true_type = integral_constant<bool, true>;
+    using false_type = integral_constant<bool, false>;
     
+    // check reference type
     template<typename>
-    struct is_lvalue_reference{
-        static const bool check = false;
-    };
+    struct is_lvalue_reference: public false_type{};
     template<typename T>
-    struct is_lvalue_reference<T&>{ // this explicitly instantiated template will be used when the argument it is an lvalue
-        static const bool check = true;
-    };
-    
+    struct is_lvalue_reference<T&>: public true_type{}; // this explicitly instantiated template will be used when the argument it is an lvalue
     template<typename>
-    struct is_rvalue_reference{
-        static const bool check = false;
-    };
+    struct is_rvalue_reference: public false_type{};
     template<typename T>
-    struct is_rvalue_reference<T&&>{ // this explicitly instantiated template will be used when the argument it is an rvalue
-        static const bool check = true;
-    };
-};
+    struct is_rvalue_reference<T&&>: public true_type{}; // this explicitly instantiated template will be used when the argument it is an rvalue
+}
 
 
 #endif //SHIVLIB_TYPETRAITS_HPP
