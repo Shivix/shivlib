961ca05f869b0cd71f43a823dd0dcf5ee765c773
Shivix <52997365+Shivix@users.noreply.github.com>
2020-09-23 16:45:19 +0100

    added iterators.hpp

---
+++ include/ShivLib/iterators.hpp
@@ -0,0 +1,18 @@
+#ifndef SHIVLIB_ITERATORS_HPP
+#define SHIVLIB_ITERATORS_HPP
+
+namespace ShivLib{
+    
+    
+    class reverse_iterator{
+        
+        
+        
+        
+        
+    };
+    
+    
+}
+
+#endif //SHIVLIB_ITERATORS_HPP
