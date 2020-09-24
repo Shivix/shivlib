1d6a8f742e68e95aa3343621d45ba1e8d262e7a0
Shivix <52997365+Shivix@users.noreply.github.com>
2020-09-24 10:26:25 +0100

    implemented iterator tags + iterator trait struct

--- include/ShivLib/iterators.hpp
+++ include/ShivLib/iterators.hpp
@@ -1,10 +1,35 @@
 #ifndef SHIVLIB_ITERATORS_HPP
 #define SHIVLIB_ITERATORS_HPP
 
+#include "cstddef.hpp"
+
 namespace ShivLib{
+    template<typename category, typename T>
+    struct iterator_traits{
+        using iterator_category = category;
+        using value_type = T; // the type the iterator points to
+        using difference_type = ptrdiff_t; // distance between operators
+        using pointer = T*;
+        using reference = T&;
+    };
+
+    template<typename category, typename T>
+    struct iterator_traits<category, const T*>{
+        using iterator_category = category;
+        using value_type = T;
+        using difference_type = ptrdiff_t;
+        using pointer = const T*;
+        using reference = const T&;
+    };
     
-    
-    class reverse_iterator{
+    struct input_iterator_tag{};
+    struct output_iterator_tag{};
+    struct forward_iterator_tag : public input_iterator_tag{};
+    struct bidirectional_iterator_tag : public forward_iterator_tag{};
+    struct random_access_iterator_tag : public bidirectional_iterator_tag{};
+
+    template<typename T>
+    class reverse_iterator: public iterator_traits<random_access_iterator_tag, T>{
         
         
         
