5d61a8a48a6f5572c767dc3e4b1592a401cf5ea6
Shivix <52997365+Shivix@users.noreply.github.com>
2020-09-30 17:39:31 +0100

    added min and max to algorithm header

--- include/ShivLib/algorithm.hpp
+++ include/ShivLib/algorithm.hpp
@@ -3,13 +3,51 @@
 
 
 namespace ShivLib{
-    class algorithm{
 
-        
-        
-        
-        
-    };  
+    template<typename T>
+    constexpr inline const T& // TODO: add concept in C++20
+    max(const T& a, const T& b){
+        if(a > b){
+            return a;
+        }
+        else{
+            return b;
+        }
+    }
+    template<typename T, typename callable>
+    constexpr inline const T&
+    max(const T& a, const T& b, callable comparisonFunc){
+        if(comparisonFunc(a > b)){
+            return a;
+        }
+        else{
+            return b;
+        }
+    }
+
+    template<typename T>
+    constexpr inline const T&
+    min(const T& a, const T& b){
+        if(a < b){
+            return a;
+        }
+        else{
+            return b;
+        }
+    }
+    template<typename T, typename callable>
+    constexpr inline const T&
+    min(const T& a, const T& b, callable comparisonFunc){
+        if(comparisonFunc(a < b)){
+            return a;
+        }
+        else{
+            return b;
+        }
+    }
+    
+    
+    
 }
 
 
