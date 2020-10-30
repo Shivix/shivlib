549c823810a1756f3f8dcf8041f394a09327eefe
Shivix <markoborne1@gmail.com>
2020-10-30 19:44:25 +0000

    added aliases

--- include/ShivLib/dataStructures/string.hpp
+++ include/ShivLib/dataStructures/string.hpp
@@ -1,10 +1,20 @@
 #ifndef SHIVLIB_STRING_HPP
 #define SHIVLIB_STRING_HPP
 
+#include <iterator>
 
 namespace ShivLib{
+    template<typename T>
     class string{
-
+        using value_type = T;
+        using pointer = T*;
+        using iterator = T*;
+        using const_iterator = const T*;
+        using reverse_iterator = std::reverse_iterator<T*>;
+        using const_reverse_iterator = const std::reverse_iterator<const T*>;
+        using reference = T&;
+        using const_reference = const T&;
+        using rvalue_reference = T&&;
         
         
         
