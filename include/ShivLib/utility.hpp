78431a28660690fbe8f22e3559a614d66a2af252
Shivix <markoborne1@gmail.com>
2020-11-07 11:30:32 +0000

    inlined free functions

--- include/ShivLib/utility.hpp
+++ include/ShivLib/utility.hpp
@@ -5,18 +5,18 @@
 
 namespace ShivLib{
     template<typename T>
-    constexpr T&& 
+    constexpr inline T&& 
     move(T&& input) noexcept { // casts a value to an rvalue to allow move semantics and other features.
         return static_cast<typename ShivLib::remove_reference<T>::type&&>(input);
     }
     
     template<typename T>
-    constexpr T&&
+    constexpr inline T&&
     forward(typename ShivLib::remove_reference<T>::type& input) noexcept {
         return static_cast<T&&>(input);
     }
     template<typename T>
-    constexpr T&& // lvalues will cannot be perfectly forwarded, so to make the behaviour explicit this function will be used and will assert at compile time
+    constexpr inline T&& // lvalues will cannot be perfectly forwarded, so to make the behaviour explicit this function will be used and will assert at compile time
     forward(typename ShivLib::remove_reference<T>::type&& input) noexcept {
         static_assert(!ShivLib::is_lvalue_reference<T>(), "Must forward an rvalue");
         return static_cast<T&&>(input);
