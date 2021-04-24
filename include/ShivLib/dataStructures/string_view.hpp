f61fa55509d5b3ad6a9768ded87615a9ad65c3d8
Shivix <markoborne1@gmail.com>
2021-04-24 15:44:57 +0100

    added initial string_view implementation

--- include/ShivLib/dataStructures/string_view.hpp
+++ include/ShivLib/dataStructures/string_view.hpp
@@ -1,11 +1,16 @@
 #ifndef SHIVLIB_STRING_VIEW_HPP
 #define SHIVLIB_STRING_VIEW_HPP
 
+#include "../concepts.hpp"
+#include <cassert>
 #include <iterator>
 
 namespace shiv {
-    template<typename T>
+    template<shiv::character T = char> // character concept
     class string_view{
+        const T* m_view;
+        size_t m_length;
+    public:
         using value_type = T;
         using pointer = T*;
         using iterator = T*;
@@ -18,9 +23,110 @@ namespace shiv {
 
         static constexpr size_t npos = size_t(-1);
 
-        size_t length;
-        const T* string;
+        constexpr string_view(): m_view{nullptr}, m_length{0} {}
+        constexpr string_view(const T* view): m_view{view}/*, m_length{strlen(m_view)} */{
+            int i{0};
+            while (view[i] != T()){ // todo: test......???do for wide char too???
+                ++i;
+            }
+            m_length = i;
+        }
+        constexpr string_view(const T* view, size_t count): m_view{view}, m_length{count} {}
+        template<typename Begin, typename End> // TODO: concepts for iterators!!!!
+        constexpr string_view(Begin begin, End end): m_view{begin}{
+            m_length = end - begin; // todo: test
+        }
+        constexpr string_view(const std::string& str): string_view(str.data()) {} // TODO: change to shiv::string or add another?
+        
+        constexpr const_iterator
+        data() const noexcept{
+            return m_view;
+        }
 
+        constexpr void
+        swap(string_view& other) noexcept{
+            auto temp_view{m_view};
+            auto temp_length{m_length};
+            m_view = other.m_view;
+            m_length = other.m_length;
+            other.m_view = temp_view;
+            other.m_length = temp_length;
+        }
+        
+        // trim m_view
+        constexpr void remove_prefix(size_t n){
+            m_view += n;
+            m_length -= n;
+        }
+        constexpr void remove_suffix(size_t n){
+            m_length -= n;
+        }
+
+        // Iterators
+        constexpr auto begin() const noexcept{
+            return const_iterator(m_view);
+        }
+        constexpr auto cbegin() const noexcept{
+            return const_iterator(m_view);
+        }
+        constexpr auto rbegin() const noexcept{
+            return const_reverse_iterator(end());
+        }
+        constexpr auto crbegin() const noexcept{
+            return const_reverse_iterator(end());
+        }
+        constexpr auto end() const noexcept{
+            return const_iterator(m_view + m_length);
+        }
+        constexpr auto cend() const noexcept{
+            return const_iterator(m_view + m_length);
+        }
+        constexpr auto rend() const noexcept{
+            return const_reverse_iterator(begin());
+        }
+        constexpr auto crend() const noexcept{
+            return const_reverse_iterator(begin());
+        }
+
+        // Element Access (read only)
+        constexpr const_reference operator[] (size_t index) const noexcept{
+            assert((index < m_length)&&("Index out of range"));
+            return m_view[index];
+        }
+
+        constexpr const_reference at(size_t index) const{
+            if(index >= m_length){
+                throw std::out_of_range("Element out of range");
+            }
+            else{
+                return m_view[index];
+            }
+        }
+
+        constexpr const_reference front() const noexcept{
+            return *begin();
+        }
+
+        constexpr const_reference back() const noexcept{
+            return *(end() - 1);
+        }
+
+        [[nodiscard]] constexpr size_t size() const noexcept{
+            return m_length;
+        }
+        [[nodiscard]] constexpr size_t length() const noexcept{
+            return m_length;
+        }
+        [[nodiscard]] constexpr size_t max_size() const noexcept{
+            return m_length;
+        }
+        [[nodiscard]] constexpr bool empty() const noexcept{
+            return m_length == 0;
+        }
+        
+        constexpr bool operator ==(const string_view& other) const noexcept{
+            return std::equal(begin(), end(), other.begin());
+        }
     };
 }
 
---
+++ test/string_view_test.cpp
@@ -0,0 +1,13 @@
+#include "../include/ShivLib/dataStructures/string_view.hpp"
+#include <boost/test/unit_test.hpp>
+#include <string>
+
+BOOST_AUTO_TEST_SUITE(string_view_test) //NOLINT
+BOOST_AUTO_TEST_CASE(trim_test){ //NOLINT
+    std::string test_string{"    Hello, world  "};
+    shiv::string_view test_view{test_string};
+    test_view.remove_prefix(4);
+    test_view.remove_suffix(2);
+    BOOST_TEST(test_view == shiv::string_view("Hello, world"));
+}
+BOOST_AUTO_TEST_SUITE_END() //NOLINT
