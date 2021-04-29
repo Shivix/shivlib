2af92dca10e3b761a5db7c4ce027f8b5970ee46a
Shivix <markoborne1@gmail.com>
2021-04-29 16:11:04 +0100

    fixed move return type

--- include/ShivLib/utility.hpp
+++ include/ShivLib/utility.hpp
@@ -5,19 +5,19 @@
 
 namespace shiv {
     template<typename T>
-    constexpr inline T&& 
+    constexpr inline shiv::remove_reference_t<T>&& 
     move(T&& input) noexcept { // casts a value to an rvalue to allow move semantics and other features.
-        return static_cast<typename shiv::remove_reference<T>::type&&>(input);
+        return static_cast<typename shiv::remove_reference_t<T>&&>(input);
     }
     
     template<typename T>
     constexpr inline T&&
-    forward(typename shiv::remove_reference<T>::type& input) noexcept {
+    forward(typename shiv::remove_reference_t<T>& input) noexcept {
         return static_cast<T&&>(input);
     }
     template<typename T>
     constexpr inline T&& // lvalues will cannot be perfectly forwarded, so to make the behaviour explicit this function will be used and will assert at compile time
-    forward(typename shiv::remove_reference<T>::type&& input) noexcept {
+    forward(typename shiv::remove_reference_t<T>&& input) noexcept {
         static_assert(!shiv::is_lvalue_reference<T>(), "Must forward an rvalue");
         return static_cast<T&&>(input);
     }
